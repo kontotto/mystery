8aafa25f4b1b6d5b16e5f08cd5b48ebe0b3b37cc
Shuta Kondo <takekon591@gmail.com>
2019-01-27 23:22:29 +0900

    add caesarone

--- caesar/caesar.go
+++ caesar/caesar.go
@@ -6,28 +6,107 @@ import (
 	"unicode/utf8"
 )
 
+type Charset struct {
+	Chars string
+	Type  CharType
+}
+
+var Number = Charset{
+	"0123456789",
+	NumberType,
+}
+
+var Alphabet = Charset{
+	"abcdefghijklmnopqrstuvwxyz",
+	AlphabetType,
+}
+
+var EmAlphabet = Charset{
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	EmAlphabetType,
+}
+
+var Hiragana = Charset{
+	"あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん",
+	HiraganaType,
+}
+
+var HiraganaDakuten = Charset{
+	"がぎぐげござじずぜぞだぢづでどばびぶべぼ",
+	HiraganaDakutenType,
+}
+
+var HiraganaHandakuten = Charset{
+	"ぱぴぷぺぽ",
+	HiraganaHandakutenType,
+}
+
+var Katakana = Charset{
+	"アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン",
+	KatakanaType,
+}
+
+var KatakanaDakuten = Charset{
+	"ガギグゲゴザジズゼゾダヂヅデドバビブベボ",
+	KatakanaDakutenType,
+}
+
+var KatakanaHandakuten = Charset{
+	"パピプペポ",
+	KatakanaHandakutenType,
+}
+
+//Number             = "0123456789"
+//Alphabet           = "abcdefghijklmnopqrstuvwxyz"
+//EmAlphabet         = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//Hiragana           = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん"
+//HiraganaDakuten    = "がぎぐげござじずぜぞだぢづでどばびぶべぼ"
+//HiraganaHandakuten = "ぱぴぷぺぽ"
+//Katakana           = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン"
+//KatakanaDakuten    = "ガギグゲゴザジズゼゾダヂヅデドバビブベボ"
+//KatakanaHandakuten = "パピプペポ"
+
+type CharType int
+
 const (
-	Number             = "0123456789"
-	Alphabet           = "abcdefghijklmnopqrstuvwxyz"
-	EmAlphabet         = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	Hiragana           = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん"
-	HiraganaDakuten    = "がぎぐげござじずぜぞだぢづでどばびぶべぼ"
-	HiraganaHandakuten = "ぱぴぷぺぽ"
-	Katakana           = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン"
-	KatakanaDakuten    = "ガギグゲゴザジズゼゾダヂヅデドバビブベボ"
-	KatakanaHandakuten = "パピプペポ"
+	Undefined CharType = iota
+	NumberType
+	AlphabetType
+	EmAlphabetType
+	HiraganaType
+	HiraganaDakutenType
+	HiraganaHandakutenType
+	KatakanaType
+	KatakanaDakutenType
+	KatakanaHandakutenType
 )
 
 const Komoji = "ぁぃぅぇぉっゃゅょァィゥェォッャュョ"
 
-//func caesarOne(s string, substr rune, offset int) (rune, error){
-//	index := strings.IndexRune(s, substr)
-//	if index == -1 {
-//		return utf8.RuneError, fmt.Errorf("%c is not found in %v", substr, s)
-//	}
-//	index = (index + offset) % utf8.RuneCountInString(s)
-//	return []rune(s)[index], nil
-//}
+func caesarOne(s string, substr rune, offset int) (rune, error) {
+	index := strings.IndexRune(s, substr)
+	if index == -1 {
+		return utf8.RuneError, fmt.Errorf("%c is not found in %v", substr, s)
+	}
+	length := utf8.RuneCountInString(s)
+	index = (index + offset) % length
+	index = (index + length) % length
+	return []rune(s)[index], nil
+}
+
+func classify(r rune) CharType {
+	charsets := []Charset{
+		Alphabet, Number, EmAlphabet,
+		Hiragana, HiraganaDakuten, HiraganaHandakuten,
+		Katakana, KatakanaDakuten, KatakanaHandakuten,
+	}
+	for _, c := range charsets {
+		if strings.ContainsRune(c.Chars, r) {
+			return c.Type
+		}
+	}
+	return Undefined
+}
 
 func normalize(src string) string {
 	normal := make([]rune, utf8.RuneCountInString(src))
@@ -49,9 +128,9 @@ func normalize(src string) string {
 func validate(str string) error {
 	all := strings.Join(
 		[]string{
-			Alphabet, Number, EmAlphabet,
-			Hiragana, HiraganaDakuten, HiraganaHandakuten,
-			Katakana, KatakanaDakuten, KatakanaHandakuten,
+			Alphabet.Chars, Number.Chars, EmAlphabet.Chars,
+			Hiragana.Chars, HiraganaDakuten.Chars, HiraganaHandakuten.Chars,
+			Katakana.Chars, KatakanaDakuten.Chars, KatakanaHandakuten.Chars,
 		},
 		"",
 	)
--- caesar/caesar_test.go
+++ caesar/caesar_test.go
@@ -2,9 +2,67 @@ package caesar
 
 import (
 	"testing"
+	"unicode/utf8"
 
 	"github.com/google/go-cmp/cmp"
 )
+
+func TestClassify(t *testing.T) {
+	t.Run("should classify for alphabet", func(t *testing.T) {
+		want := AlphabetType
+		got := classify('a')
+
+		if !cmp.Equal(want, got) {
+			t.Fatalf("want: %#v, got: %#v", want, got)
+		}
+	})
+
+	t.Run("should not for kanji", func(t *testing.T) {
+		want := Undefined
+		got := classify('心')
+
+		if !cmp.Equal(want, got) {
+			t.Fatalf("want: %#v, got: %#v", want, got)
+		}
+	})
+}
+
+func TestCaesarOne(t *testing.T) {
+	t.Run("should 1 forwarder", func(t *testing.T) {
+		want := 'a'
+		got, err := caesarOne(Alphabet.Chars, 'z', 1)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !cmp.Equal(want, got) {
+			t.Fatalf("want: %#v, got: %#v", want, got)
+		}
+	})
+
+	t.Run("should 1 backwarder", func(t *testing.T) {
+		want := 'z'
+		got, err := caesarOne(Alphabet.Chars, 'a', -1)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !cmp.Equal(want, got) {
+			t.Fatalf("want: %#v, got: %#v", want, got)
+		}
+	})
+
+	t.Run("should fail", func(t *testing.T) {
+		want := utf8.RuneError
+		got, err := caesarOne(Alphabet.Chars, 'A', -1)
+
+		if err == nil {
+			t.Fatalf("want: %#v, got: %#v", want, got)
+		}
+	})
+}
 
 func TestNormalize(t *testing.T) {
 	t.Run("should upper hiragana and katakana", func(t *testing.T) {
